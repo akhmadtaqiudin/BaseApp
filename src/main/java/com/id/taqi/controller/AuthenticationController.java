b0c197b669a3c5a6f3e61b9d11bbc2533ae1eb1a
bayufn <bayufajarnugraha_28@yahoo.com>
2020-05-02 14:21:37 +0700

    add mapping users

--- src/main/java/com/id/taqi/controller/AuthenticationController.java
+++ src/main/java/com/id/taqi/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.id.taqi.controller;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.validation.Valid;
@@ -14,6 +15,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +35,7 @@ import com.id.taqi.services.UserService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 public class AuthenticationController {
 
 	@Autowired
@@ -63,22 +66,22 @@ public class AuthenticationController {
 	}
 
 	@PostMapping("/signup")
-	public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpForm signUpRequest) {
-		if (userService.existsByUserName(signUpRequest.getUserName())) {
+	public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpForm req) {
+		if (userService.existsByUserName(req.getUserName())) {
 			return new ResponseEntity<>(new ResponseMessage("Fail -> Username is already taken!"),
 					HttpStatus.BAD_REQUEST);
 		}
 
-		if (userService.existsByEmail(signUpRequest.getEmail())) {
+		if (userService.existsByEmail(req.getEmail())) {
 			return new ResponseEntity<>(new ResponseMessage("Fail -> Email is already in use!"),
 					HttpStatus.BAD_REQUEST);
 		}
 
 		// Creating user's account
-		User user = new User(signUpRequest.getName(), signUpRequest.getUserName(), signUpRequest.getEmail(),
-				encoder.encode(signUpRequest.getPassword()));
+		User user = new User(req.getName(), req.getUserName(), req.getEmail(),
+				encoder.encode(req.getPassword()));
 
-		Set<String> strRoles = signUpRequest.getRole();
+		Set<String> strRoles = req.getRole();
 		Set<Role> roles = new HashSet<>();
 
 		strRoles.forEach(role -> {
@@ -101,5 +104,25 @@ public class AuthenticationController {
 		userService.save(user);
 
 		return new ResponseEntity<>(new ResponseMessage("User registered successfully!"), HttpStatus.OK);
+	}
+
+	@GetMapping("/users")
+	public ResponseEntity<List<User>> getAllUsers(){		
+		try {
+			List<User> user = userService.getAll();
+			return ResponseEntity.ok(user);
+		}catch (Exception e){			
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+	}
+	
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUserBYId(@PathVariable("id") Long id){		
+		try {
+			User user = userService.getById(id);
+			return ResponseEntity.ok(user);
+		}catch (Exception e){			
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
 	}
 }
